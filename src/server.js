30bd413d0bc822b7c0465ed45ac82fa14d97931b
Kaato <34705627+kuhiga@users.noreply.github.com>
2021-02-23 22:18:15 -0800

    make it better

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
-
+const express = require('express');
 const app = express();
+const path = require('path');
 
-//here we are configuring dist to serve app files
-app.use("/", serveStatic(path.join(__dirname, "/dist/")));
+app.use(express.static(__dirname + '/dist/flute-salad'));
+app.listen(process.env.PORT || 8080);
 
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
-});
+//PATH LOCATION STARTEGY
 
-const port = process.env.PORT || 8080;
-app.listen(port);
-console.log(`app is listening on port: ${port}`);
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/dist/flute-salad/index.html');
+  console.log(" Fetching from.." + fullPath);
+    res.sendFile(fullPath);
+})
+
+console.log('Server started running..');
+
+//Changed to run on Heroku
