9ed20e1145b359e34b4932adc8569e56f78ff823
Kaato <34705627+kuhiga@users.noreply.github.com>
2021-05-06 12:24:34 -1000

    footer section styling

--- src/components/footerSection.vue
+++ src/components/footerSection.vue
@@ -19,10 +19,10 @@ export default {
 #footerSection {
   color: Black;
   text-align: left;
-  position: absolute;
+  /* position: absolute;
   bottom: 0;
   width: 100%;
-  height: 60px; /* Height of the footer */
+  height: 60px; Height of the footer */
 }
 hr {
   width: 100%;
